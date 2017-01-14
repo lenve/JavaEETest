4870ea6a921e28ff1e259d5458c483fc8b13667f
sang <wangsong0210@gmail.com>
2017-01-14 23:55:24 +0800

    mybatis常用配置案例

---
+++ Test27-mybatis4/src/main/java/org/sang/Main.java
@@ -0,0 +1,93 @@
+package org.sang;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.sang.bean.User;
+import org.sang.db.DBUtils;
+import org.sang.db.UserMapper;
+
+import java.util.List;
+
+/**
+ * Created by sang on 17-1-14.
+ */
+public class Main {
+    @Test
+    public void test1() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = DBUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.getUser(1l);
+            System.out.println(user);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+
+    }
+
+    @Test
+    public void test2() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = DBUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            for (int i = 0; i < 3; i++) {
+                userMapper.insertUser(new User(null, "u-" + i, "p-" + i, "a-" + i));
+            }
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void test3() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = DBUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            int i = userMapper.deleteUser(5l);
+            System.out.println(i);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+    @Test
+    public void test4() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = DBUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<User> all = userMapper.getAll();
+            for (User user : all) {
+                System.out.println(user);
+            }
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+}
